a4dad5744e5f58ccf2aadeba5a91b7cc4474b937
Thorsten Eggeling <Myria-de@users.noreply.github.com>
2020-01-23 19:12:24 +0100

    Add files via upload

--- gettemp_console.py
+++ gettemp_console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+# Beispiel Seite 80/81
+# Temperatur messen mit DS18S20
 import time
 # den Pfad bitte anpassen
 sensor = '/sys/bus/w1/devices/28-031797798836/w1_slave'
