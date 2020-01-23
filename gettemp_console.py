a7c6ed84171fe7f531a0a3fc923902cd8a8ba3b0
Thorsten Eggeling <Myria-de@users.noreply.github.com>
2020-01-23 19:08:18 +0100

    Add files via upload

---
+++ gettemp_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import time
+# den Pfad bitte anpassen
+sensor = '/sys/bus/w1/devices/28-031797798836/w1_slave'
+def readTempSensor(sensorName) :
+    f = open(sensorName, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+while True :
+    print(time.strftime('%H:%M:%S') +" - " + str(readTempSensor(sensor)))
+    time.sleep(10)
